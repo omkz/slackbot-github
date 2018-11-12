ed3584a1942fc3356e35282f6e55c1e4f004d8ae
Kurnia Muhamad <omenkzz@gmail.com>
2018-11-12 15:00:17 +0700

    send a notification to a slack channel when a label is added to an issue

--- app.rb
+++ app.rb
@@ -1,7 +1,32 @@
 require 'sinatra'
 require 'json'
+require 'httparty'
+require 'dotenv/load'
 
 post '/payload' do
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+  event_type = request.env['HTTP_X_GITHUB_EVENT']
+  request.body.rewind
+  payload = JSON.parse(request.body.read)
+  # pp payload
+
+  case event_type
+  when "issues"
+    process_issues(payload)
+  else
+    puts "Oooh, something new from GitHub: #{event_type}"
+  end
 end
+
+def process_issues(payload)
+  url = ENV['SLACK_WEBHOOK_URL']
+  if payload['action'] == 'labeled'
+    HTTParty.post(
+      url,
+      headers: { 'Content-Type' => 'application/json' },
+      body: {
+        text: 'Label ' + payload['label']['name'] + ' was added to issue ' + '<' + payload['issue']['html_url'] + '|' + payload['issue']['number'].to_s + '>'
+      }.to_json
+    )
+  end
+end
+
